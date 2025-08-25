ccbd757bd3db0d073bb4de6e8f36a457951ff451
Rian Wilker Santos Melo <78122002+RWilker87@users.noreply.github.com>
2025-08-25 16:53:32 -0300

    Customização para retornar um JSON com erro

---
+++ java-spring-projeto/src/main/java/br/com/projeto/exception/handler/CustomEntityResponseHandler.java
@@ -0,0 +1,36 @@
+package br.com.projeto.exception.handler;
+
+import br.com.projeto.exception.ExceptionResponse;
+import br.com.projeto.exception.UnsupportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+
+@RestController
+@ControllerAdvice
+public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExcpetions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExcpetions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
